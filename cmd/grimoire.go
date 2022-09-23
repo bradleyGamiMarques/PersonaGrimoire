96d6530ab8cf57a57d2ecc52f969f530854d8a93
Bradley Marques <bradleygamimarques@outlook.com>
2022-09-23 03:46:25 -0700

    Add functions for generating database tables

---
+++ cmd/grimoire.go
@@ -0,0 +1,29 @@
+package grimoire
+
+import (
+	"github.com/bradleyGamiMarques/PersonaGrimoire/api"
+	"github.com/sirupsen/logrus"
+	"gorm.io/gorm"
+)
+
+func RegisterDatabaseComponents(gorm *gorm.DB, logger *logrus.Logger) {
+	migrationError := gorm.AutoMigrate(&api.Arcana{})
+	if migrationError != nil {
+		logger.Panicf("gorm Migration Error for Arcana: %v", migrationError)
+	}
+
+	migrationError = gorm.AutoMigrate(&api.P5Persona{})
+	if migrationError != nil {
+		logger.Panicf("gorm Migration Error for P5Persona: %v", migrationError)
+	}
+
+	migrationError = gorm.AutoMigrate(&api.P5PersonaSkill{})
+	if migrationError != nil {
+		logger.Panicf("gorm Migration Error for P5PersonaSkill: %v", migrationError)
+	}
+
+	migrationError = gorm.AutoMigrate(&api.P5PersonaStats{})
+	if migrationError != nil {
+		logger.Panicf("gorm Migration Error for P5PersonaSkill: %v", migrationError)
+	}
+}
