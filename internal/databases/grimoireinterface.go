32fa4496714f5839996c1b861af8d4407ba425c0
Bradley Marques <bradleygamimarques@outlook.com>
2022-09-23 21:44:29 -0700

    Add method to check if Arcana exists by name

--- internal/databases/grimoireinterface.go
+++ internal/databases/grimoireinterface.go
@@ -8,4 +8,5 @@ import (
 
 type PersonaGrimoire interface {
 	CheckIfArcanaExistsByUUID(ctx context.Context, arcanaID uuid.UUID) (exists bool, err error)
+	CheckIfArcanaExistsByName(ctx context.Context, arcanaName string) (exists bool, err error)
 }
--- internal/databases/grimoireinterfaceimpl.go
+++ internal/databases/grimoireinterfaceimpl.go
@@ -15,9 +15,18 @@ type PersonaGrimoireImpl struct {
 	Logger *logrus.Logger
 }
 
+// Check if something exists.
 func (p *PersonaGrimoireImpl) CheckIfArcanaExistsByUUID(ctx context.Context, arcanaID uuid.UUID) (exists bool, err error) {
 	var count int64
 	err = p.Gorm.WithContext(ctx).Model(&api.Arcana{}).Where(&api.Arcana{ArcanaID: arcanaID}).Count(&count).Error
+	if err != nil {
+		return false, fmt.Errorf("failed to count arcanas Error: %w", err)
+	}
+	return count > 0, fmt.Errorf("NOT IMPLEMENTED")
+}
+func (p *PersonaGrimoireImpl) CheckIfArcanaExistsByName(ctx context.Context, arcanaName string) (exists bool, err error) {
+	var count int64
+	err = p.Gorm.WithContext(ctx).Model(&api.Arcana{}).Where(&api.Arcana{ArcanaName: arcanaName}).Count(&count).Error
 	if err != nil {
 		return false, fmt.Errorf("failed to count arcanas Error: %w", err)
 	}
