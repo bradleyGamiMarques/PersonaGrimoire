7b20d9d91f759e894caeb33615bff8a7c1cf9433
Bradley Marques <bradleygamimarques@outlook.com>
2022-09-24 16:13:37 -0700

    Add implementation of GetPersona5ArcanaByUUID

--- cmd/grimoireapi/routerArcanas.go
+++ cmd/grimoireapi/routerArcanas.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/bradleyGamiMarques/PersonaGrimoire/api"
 )
@@ -10,21 +11,36 @@ func (c *Container) GetPersona5ArcanaByName(ctx context.Context, request api.Get
 	// Check if Arcana exists.
 	exists, err := c.PersonaGrimoire.CheckIfArcanaExistsByName(ctx, request.ArcanaName)
 	if err != nil {
-		c.Logger.Errorf("Error attempting to check if Arcana exists Arcana Name: %s Error: %s", request.ArcanaName, err.Error())
-		return api.ServerErrorJSONResponse{}, nil
+		c.Logger.Errorf("Error attempting to check if Arcana exists. Arcana Name: %s Error: %s", request.ArcanaName, err.Error())
+		return api.ServerErrorJSONResponse{Code: 500, Error: true, Message: "Error attempting to check if Arcana exists."}, nil
 	}
 	if !exists {
-		c.Logger.Warnf("Attempted to get Arcana by name that does not exist Arcana Name: %s", request.ArcanaName)
+		c.Logger.Warnf("Attempted to get Arcana by name that does not exist. Arcana Name: %s", request.ArcanaName)
+		return api.NotFoundJSONResponse{Code: 404, Error: true, Message: fmt.Sprintf("Attempted to get Arcana by Name that does not exist. Arcana Name: %s", request.ArcanaName)}, nil
 	}
 	arcana, err := c.PersonaGrimoire.GetPersona5ArcanaByName(ctx, request.ArcanaName)
-	c.Logger.Infof("Arcana: %v", arcana)
 	if err != nil {
-		c.Logger.Errorf("Error attempting to get Arcana by name Arcana Name: %s Error %s", request.ArcanaName, err.Error())
-		return api.ServerErrorJSONResponse{}, nil
+		c.Logger.Errorf("Error attempting to get Arcana by name. Arcana Name: %s Error %s", request.ArcanaName, err.Error())
+		return api.ServerErrorJSONResponse{Code: 500, Error: true, Message: fmt.Sprintf("Error attempting to get Arcana by Name. Arcana Name: %s", request.ArcanaName)}, nil
 	}
 	return api.GetPersona5ArcanaByName200JSONResponse(arcana), nil
 }
 
 func (c *Container) GetPersona5ArcanaByUUID(ctx context.Context, request api.GetPersona5ArcanaByUUIDRequestObject) (api.GetPersona5ArcanaByUUIDResponseObject, error) {
-	return nil, nil
+	// Check if Arcana exists.
+	exists, err := c.PersonaGrimoire.CheckIfArcanaExistsByUUID(ctx, request.ArcanaUUID)
+	if err != nil {
+		c.Logger.Errorf("Error attempting to check if Arcana exists. Arcana ID: %s Error: %s", request.ArcanaUUID, err.Error())
+		return api.ServerErrorJSONResponse{Code: 500, Error: true, Message: "Error attempting to check if Arcana exists."}, nil
+	}
+	if !exists {
+		c.Logger.Warnf("Attempted to get Arcana by ID that does not exist. Arcana ID: %s", request.ArcanaUUID)
+		return api.NotFoundJSONResponse{Code: 404, Error: true, Message: fmt.Sprintf("Attempted to get Arcana by ID that does not exist. Arcana ID: %s", request.ArcanaUUID)}, nil
+	}
+	arcana, err := c.PersonaGrimoire.GetPersona5ArcanaByUUID(ctx, request.ArcanaUUID)
+	if err != nil {
+		c.Logger.Errorf("Error attempting to get Arcana by ID. Arcana ID: %s Error %s", request.ArcanaUUID, err.Error())
+		return api.ServerErrorJSONResponse{Code: 500, Error: true, Message: fmt.Sprintf("Error attempting to get Arcana by ID. Arcana ID: %s", request.ArcanaUUID)}, nil
+	}
+	return api.GetPersona5ArcanaByName200JSONResponse(arcana), nil
 }
