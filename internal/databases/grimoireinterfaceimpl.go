258e6a6138d648d75800c3370f3c718e7fef6237
Bradley Marques <bradleygamimarques@outlook.com>
2022-09-23 21:40:34 -0700

    Add method to check if Arcana exists by UUID

--- internal/databases/grimoireinterfaceimpl.go
+++ internal/databases/grimoireinterfaceimpl.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/bradleyGamiMarques/PersonaGrimoire/api"
 	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
 	"gorm.io/gorm"
@@ -14,6 +15,11 @@ type PersonaGrimoireImpl struct {
 	Logger *logrus.Logger
 }
 
-func (p *PersonaGrimoireImpl) CheckIfArcanaExists(ctx context.Context, arcanaID uuid.UUID) (exists bool, err error) {
-	return false, fmt.Errorf("NOT IMPLEMENTED")
+func (p *PersonaGrimoireImpl) CheckIfArcanaExistsByUUID(ctx context.Context, arcanaID uuid.UUID) (exists bool, err error) {
+	var count int64
+	err = p.Gorm.WithContext(ctx).Model(&api.Arcana{}).Where(&api.Arcana{ArcanaID: arcanaID}).Count(&count).Error
+	if err != nil {
+		return false, fmt.Errorf("failed to count arcanas Error: %w", err)
+	}
+	return count > 0, fmt.Errorf("NOT IMPLEMENTED")
 }
